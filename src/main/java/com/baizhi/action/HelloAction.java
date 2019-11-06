cfbb6c7aab581da0ffdffb5d3eefec347988f375
GuoYabin130 <57441368+GuoYabin130@users.noreply.github.com>
2019-11-06 20:41:43 +0800

    Update HelloAction.java

--- src/main/java/com/baizhi/action/HelloAction.java
+++ src/main/java/com/baizhi/action/HelloAction.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/hello")
 public class HelloAction {
 
+    //这是同事修改过得
     @RequestMapping("/boot")
     @ResponseBody
     public String boot() throws Exception{
